8813aca7339f1cf67bcce2bd4c5b135371a0f27b
changlei <changlei@graspyun.com>
2020-10-23 13:25:27 +0800

    添加macos和linux支持

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import path_provider_macos
+import preimage
+import sqflite
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  PreimagePlugin.register(with: registry.registrar(forPlugin: "PreimagePlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+}
