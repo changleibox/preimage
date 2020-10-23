c0924f0cc41c09677d9f39f9e91f80fa822c1633
changlei <changlei@graspyun.com>
2020-10-23 14:02:50 +0800

    添加macos和linux支持

--- lib/preimage.dart
+++ lib/preimage.dart
@@ -1,6 +1,6 @@
 library preimage;
 
-export 'src/preimage_page.dart' show Preimage;
+export 'src/preimage_page.dart' show Preimage, PreimageNavigationBarBuilder;
 export 'src/preimage_view.dart';
 export 'src/primitive_navigation_bar.dart';
 export 'src/support_activity_indicator.dart';
