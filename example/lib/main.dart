c77950c9a01092ceae65263d9accc40744f7b7b1
changlei <changlei@graspyun.com>
2020-10-29 15:29:36 +0800

    修改demo

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -23,75 +23,91 @@ class _MyAppState extends State<MyApp> {
 }
 
 class _PluginExamplePage extends StatelessWidget {
+  Widget _buildBottomBar(BuildContext context, int index) {
+    if (index % 4 == 1) {
+      return SizedBox.shrink();
+    }
+    return Container(
+      height: index.isEven ? null : MediaQuery.of(context).size.height / 2,
+      padding: EdgeInsets.symmetric(
+        horizontal: 16,
+        vertical: 10,
+      ),
+      child: SafeArea(
+        top: false,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '测试标题',
+              style: TextStyle(
+                color: CupertinoDynamicColor.resolve(
+                  CupertinoColors.label,
+                  context,
+                ),
+              ),
+            ),
+            Text(
+              '测试内容',
+              style: TextStyle(
+                fontSize: 15,
+                color: CupertinoDynamicColor.resolve(
+                  CupertinoColors.secondaryLabel,
+                  context,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final images = List.generate(10, (index) {
+      return ImageOptions(
+        url: testAvatarUrl,
+        tag: [testAvatarUrl, index].join('_'),
+      );
+    });
     return CupertinoPageScaffold(
       navigationBar: CupertinoNavigationBar(
         middle: const Text('Plugin example app'),
       ),
       child: Center(
-        child: CupertinoButton(
-          borderRadius: BorderRadius.zero,
-          padding: EdgeInsets.zero,
-          onPressed: () {
-            Preimage.preview(
-              context,
-              images: List.generate(10, (index) {
-                return ImageOptions(
-                  url: testAvatarUrl,
-                  tag: testAvatarUrl,
-                );
-              }),
-              bottomBarBuilder: (context, int index) {
-                if (index % 4 == 1) {
-                  return SizedBox.shrink();
-                }
-                return Container(
-                  height: index.isEven ? null : MediaQuery.of(context).size.height / 2,
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 16,
-                    vertical: 10,
-                  ),
-                  child: SafeArea(
-                    top: false,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          '测试标题',
-                          style: TextStyle(
-                            color: CupertinoDynamicColor.resolve(
-                              CupertinoColors.label,
-                              context,
-                            ),
-                          ),
-                        ),
-                        Text(
-                          '测试内容',
-                          style: TextStyle(
-                            fontSize: 15,
-                            color: CupertinoDynamicColor.resolve(
-                              CupertinoColors.secondaryLabel,
-                              context,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
+        child: GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 5,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+          ),
+          itemCount: images.length,
+          itemBuilder: (context, index) {
+            final image = images[index];
+            return CupertinoButton(
+              borderRadius: BorderRadius.zero,
+              padding: EdgeInsets.zero,
+              minSize: 0,
+              onPressed: () {
+                Preimage.preview(
+                  context,
+                  images: images,
+                  initialIndex: index,
+                  bottomBarBuilder: _buildBottomBar,
                 );
               },
+              child: PreimageHero(
+                tag: image.tag,
+                child: CachedNetworkImage(
+                  imageUrl: image.url,
+                  width: double.infinity,
+                  height: double.infinity,
+                  fit: BoxFit.cover,
+                ),
+              ),
             );
           },
-          child: PreimageHero(
-            tag: testAvatarUrl,
-            child: CachedNetworkImage(
-              imageUrl: testAvatarUrl,
-              width: 200,
-              height: 200,
-              fit: BoxFit.cover,
-            ),
-          ),
         ),
       ),
     );
