75c807d6fdcc5e436a6c624f6e6f11828513dd95
changlei <changlei@graspyun.com>
2021-08-03 16:36:09 +0800

    feat: 增加视频显示用例

--- lib/preimage.dart
+++ lib/preimage.dart
@@ -4,7 +4,8 @@
 
 library preimage;
 
+export 'src/preimage_gallery.dart';
 export 'src/preimage_page.dart' show Preimage, PreimageNavigationBarBuilder, Edge, ImageOptions, PreimageHero;
 export 'src/preimage_route.dart';
-export 'src/preimage_gallery.dart';
 export 'src/primitive_navigation_bar.dart';
+export 'src/vertigo_preview.dart';
